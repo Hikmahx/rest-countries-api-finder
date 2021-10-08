f9df87600ced975129168c268aff9f595168e4c8
Hikmahx <hikmayousuph@gmail.com>
2021-10-08 23:24:54 +0100

    search component updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className="search-container py-4 px-8 bg-white lg:max-w-lg shadow-md rounded flex items-center">
+    <div className="search-container mb-12 py-4 px-8 bg-white lg:w-3/4 lg:max-w-lg shadow-md rounded flex items-center">
       <ion-icon class="text-dark-gray-light text-xl" name="search"></ion-icon>
       <input className="focus:outline-none font-NunitoSans ml-5 w-4/5" type="text" placeholder="Search for a country..." />
     </div>

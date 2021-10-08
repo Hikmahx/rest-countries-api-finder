80edbbdc6d997636f56047e2c4145e6590c50c9c
Hikmahx <hikmayousuph@gmail.com>
2021-10-08 21:53:03 +0100

    background color added to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-very-light-gray-light dark:bg-very-dark-blue-dark">
       <Navbar/>
       <Main/>
     </div>

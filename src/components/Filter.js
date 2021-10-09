651c43e4436d13eac5c669346b006b80f2c328ae
Hikmahx <hikmayousuph@gmail.com>
2021-10-09 14:42:25 +0100

    filter component completed

---
+++ src/components/Filter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div className="filter-wrapper w-1/2 bg-white relative mb-12 py-3 font-NunitoSans lg:w-1/6 h-14 max-w-xs shadow-md rounded flex items-center">
+        <select className="form-select block  mt-1 px-2 text-very-dark-blue-light absolute right-0 w-full top-3 focus:outline-none">
+        <option value="region">Filter by Region</option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+        </select>
+    </div>
+  )
+}
+
+export default Filter

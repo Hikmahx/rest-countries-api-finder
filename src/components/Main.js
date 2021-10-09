686ec4ecd2d302d9023d484a2dad3717735ebe59
Hikmahx <hikmayousuph@gmail.com>
2021-10-09 23:08:40 +0100

    countries component and props added to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Search from './Search'
 import Filter from './Filter'
+import Countries from './Countries'
 
-const Main = () => {
+
+const Main = ({countries}) => {
   return (
-    <main className="p-6 sm:px-8 md:px-12 xl:px-20 pt-12 h-screen">
+    <main className="font-NunitoSans p-6 sm:px-8 md:px-12 xl:px-20 pt-12 h-screen">
       <div className="input-wrapper flex flex-wrap items-center justify-between">
         <Search/>
         <Filter />
       </div>
-
+      <Countries countries={countries}/>
     </main>
   )
 }

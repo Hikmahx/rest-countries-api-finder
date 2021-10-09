2983199a01183f2432b5401196edb2666c306993
Hikmahx <hikmayousuph@gmail.com>
2021-10-09 14:43:01 +0100

    filter and search imported

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Search from './Search'
+import Filter from './Filter'
 
 const Main = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="p-6 sm:px-8 md:px-12 xl:px-20 pt-12 h-screen">
+      <div className="input-wrapper flex flex-wrap items-center justify-between">
+        <Search/>
+        <Filter />
+      </div>
+
+    </main>
   )
 }
 

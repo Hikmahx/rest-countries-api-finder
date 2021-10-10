e419750a8e3e480d98eae36dd0d96b6e08cc0b0b
Hikmahx <hikmayousuph@gmail.com>
2021-10-10 23:27:15 +0100

    key up event added to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({searchInput}) => {
   return (
     <div className="search-container mb-12 dark:bg-dark-blue-dark py-4 px-8 bg-white lg:w-3/4 lg:max-w-lg shadow-md rounded flex items-center">
       <ion-icon class="text-dark-gray-light dark:text-white text-xl" name="search"></ion-icon>
-      <input className="focus:outline-none font-NunitoSans dark:bg-dark-blue-dark ml-5 w-4/5" type="text" placeholder="Search for a country..." />
+      <input onKeyUp={searchInput} className="focus:outline-none font-NunitoSans dark:bg-dark-blue-dark ml-5 w-4/5" type="text" placeholder="Search for a country..." />
     </div>
   )
 }

ed19cec6fd0282f7ee450fc248c5844eda3b30dd
Hikmahx <hikmayousuph@gmail.com>
2021-10-10 23:31:28 +0100

    searchInput props added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,12 @@ import Filter from './Filter'
 import Countries from './Countries'
 
 
-const Main = ({countries}) => {
+const Main = ({countries, searchInput}) => {
   return (
     <main className="font-NunitoSans p-6 sm:px-8 md:px-12 xl:px-20 pt-12 pb-20 lg:pb-11 min-h-screen dark:text-white">
       <div className="input-wrapper flex flex-wrap items-center justify-between">
-        <Search/>
-        <Filter />
+        <Search searchInput={searchInput}/>
+        <Filter/>
       </div>
       <Countries countries={countries}/>
     </main>

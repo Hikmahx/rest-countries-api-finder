b5e38c3835478f6de7595cad49a9c05f9c6ebd04
Hikmahx <hikmayousuph@gmail.com>
2021-10-11 19:25:50 +0100

    trycatch added to countries and error reponse returned if  failed to fetch or not found

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,12 +2,21 @@ import React from 'react'
 import CountryItem from './CountryItem'
 import Loading from './Loading'
 
-const Countries = ({countries, loading}) => {
+const Countries = ({countries, loading, error, errMessage}) => {
+  if(error===true){
+    return(
+      <div>
+        <h1 className="text-xl lg:text-4xl text-center">{errMessage}</h1>
+      </div>
+    )
+  }else{
+
   if(loading ===true){
     return(
       <Loading/>
     )
   }else{
+    try{
   return (
     <div style={{rowGap:'4.6rem', marginBottom: '2.5px'}} className="countries-wrapper max-w-lg m-auto sm:max-w-xl lg:max-w-none sm:px-12 lg:p-0 flex flex-wrap justify-between lg:grid lg:grid-cols-3 xl:grid-cols-4 gap-x-20">
       {countries.map((country)=>(
@@ -15,7 +24,16 @@ const Countries = ({countries, loading}) => {
       )
       )}     
     </div>
-  )
+  )      
+    }catch(err){
+      return(
+        <div>
+          <h1 className="text-xl lg:text-4xl text-center">Country {countries.message}</h1>
+        </div>
+      )
+    }
+
+  }
   }
 }
 

77f4e57b40d954412808f794bcec297dc8066496
Hikmahx <hikmayousuph@gmail.com>
2021-10-11 20:07:05 +0100

    files formatted with prettier

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import Search from './Search'
-import Filter from './Filter'
-import Countries from './Countries'
+import React from 'react';
+import Search from './Search';
+import Filter from './Filter';
+import Countries from './Countries';
 
-
-const Main = ({countries, loading, error, errMessage, searchInput, emptyInput}) => {
+const Main = ({
+  countries,
+  loading,
+  error,
+  errMessage,
+  searchInput,
+  emptyInput,
+}) => {
   return (
     <main className='font-NunitoSans relative p-6 sm:px-8 md:px-12 xl:px-20 pt-12 pb-20 lg:pb-11 min-h-screen dark:text-white'>
       <div className='input-wrapper flex flex-wrap items-center justify-between'>
-        <Search searchInput={searchInput}  emptyInput={emptyInput}/>
+        <Search searchInput={searchInput} emptyInput={emptyInput} />
         <Filter />
       </div>
-      <Countries countries={countries} loading={loading} error={error} errMessage={errMessage} />
+      <Countries
+        countries={countries}
+        loading={loading}
+        error={error}
+        errMessage={errMessage}
+      />
     </main>
   );
-}
+};
 
-export default Main
+export default Main;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Search = ({searchInput, emptyInput}) => {
+const Search = ({ searchInput, emptyInput }) => {
   return (
-    <div className="search-container mb-12 dark:bg-dark-blue-dark py-4 px-8 bg-white lg:w-3/4 lg:max-w-lg shadow-md rounded flex items-center">
-      <ion-icon class="text-dark-gray-light dark:text-white text-xl" name="search"></ion-icon>
+    <div className='search-container mb-12 dark:bg-dark-blue-dark py-4 px-8 bg-white lg:w-3/4 lg:max-w-lg shadow-md rounded flex items-center'>
+      <ion-icon
+        class='text-dark-gray-light dark:text-white text-xl'
+        name='search'
+      ></ion-icon>
       <form onSubmit={searchInput}>
-        <input onKeyUp={emptyInput} className="focus:outline-none font-NunitoSans dark:bg-dark-blue-dark ml-5 w-4/5" type="text" placeholder="Search for a country..." />
+        <input
+          onKeyUp={emptyInput}
+          className='focus:outline-none font-NunitoSans dark:bg-dark-blue-dark ml-5 w-4/5'
+          type='text'
+          placeholder='Search for a country...'
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;

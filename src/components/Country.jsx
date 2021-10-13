38efcdc522d48ac63a4ca67f44a079e101fab624
Hikmahx <hikmayousuph@gmail.com>
2021-10-13 18:41:47 +0100

    link removed

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Loading from './Loading';
 
 const Country = ({ country, loading, getBorderCountry, error, errMessage }) => {

967f355cb243dbf5e835097ed2459a897266ebed
Hikmahx <hikmayousuph@gmail.com>
2021-10-13 11:19:06 +0100

    document formatted

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -1,28 +1,33 @@
-import { Fragment, useEffect} from 'react'
-import Country from '../Country'
-import Navbar from '../Navbar'
+import { Fragment, useEffect } from 'react';
+import Country from '../Country';
+import Navbar from '../Navbar';
 
-// 'https://restcountries.com/v3.1/name/nigeria?fullText=true'
-
-const Details = ({modeToggle, country, setCountry, loading, error, errMessage}) => {
-  document.title = 'Country Details'
+const Details = ({
+  modeToggle,
+  country,
+  setCountry,
+  loading,
+  error,
+  errMessage,
+}) => {
+  document.title = 'Country Details';
 
   useEffect(() => {
-    setCountry(JSON.parse(localStorage.getItem('country')))
+    setCountry(JSON.parse(localStorage.getItem('country')));
     // eslint-disable-next-line
-  }, [])
-
+  }, []);
 
   return (
     <Fragment>
-      <Navbar modeToggle={modeToggle}/>
-      <Country country={country}            
-       loading={loading}
-       error={error}
-       errMessage= {errMessage}
-/>
+      <Navbar modeToggle={modeToggle} />
+      <Country
+        country={country}
+        loading={loading}
+        error={error}
+        errMessage={errMessage}
+      />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;

be7b1f0a80b01ef6e9e2a47e0b045532293cc921
Hikmahx <hikmayousuph@gmail.com>
2021-10-13 20:16:56 +0100

    attribution added to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,6 +26,14 @@ const Main = ({
         errMessage={errMessage}
         getCountry={getCountry}
       />
+    <div className="attribution absolute h-auto bottom-3 lg:bottom-0 left-0 right-0 transition-colors px-6 sm:px-8 md:px-12 xl:px-20 w-full mx-auto dark:text-white flex flex-wrap items-center justify-center text-center">
+      <span>      
+        Challenge by <a className='text-dark-gray-light underline hover:text-dark-blue-dark dark:hover:text-white' href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.  
+        Coded by <a className='text-dark-gray-light underline hover:text-dark-blue-dark dark:hover:text-white'  href="https://github.com/Hikmahx">Hikmah</a>.
+      </span>
+    </div>
+
+
     </main>
   );
 };
